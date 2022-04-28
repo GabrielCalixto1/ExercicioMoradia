14e5676ee30c98cc5b5ac2868ba11b7817f42334
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-04-28 15:53:02 -0300

    Iniciando XUnit

---
+++ ProjetoMoradia.TesteUnitario/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using ExercicioMoradia.Models;
+
+namespace ProjetoMoradia.TesteUnitario;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var home = new Home();
+    }
+}
